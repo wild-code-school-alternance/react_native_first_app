52070fc3d4e685c5f24d6bbf35aaf118340664ce
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2023-01-23 17:08:06 +0100

    add somme component

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import CameraScreen from "./screens/CameraScreen";
+import FeedScreen from "./screens/FeedScreen";
+import ImagesScreen from "./screens/ImagesScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Camera") {
+              iconName = focused ? "camera" : "camera-outline";
+            } else if (route.name === "Images") {
+              iconName = focused ? "image" : "image-outline";
+            } else if (route.name === "Feed") {
+              iconName = focused ? "share-social" : "share-social-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "grey",
+        })}
+      >
+        <Tab.Screen name="Camera" component={CameraScreen} options={{ unmountOnBlur: true }} />
+        <Tab.Screen name="Images" component={ImagesScreen} />
+        <Tab.Screen name="Feed" component={FeedScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
