8b22d01122703412bc97835ad34cedeaca85361b
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2023-01-24 14:58:42 +0100

    quete 3

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,7 +1,49 @@
-import React from "react";
-import {Text} from "react-native";
+import React, { useRef } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Camera } from "expo-camera";
 
 export default function CameraScreen() {
-    return <Text>Camera Screen</Text>;
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+    const cameraRef = useRef(null);
+
+    if (!permission) {
+        return <View />;
+    }
+    if (!permission.granted) {
+        return (
+            <View style={styles.container}>
+                <Text style={{ textAlign: "center" }}>
+                    We need your permission to show the camera
+                </Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+    return (
+        <>
+            <Camera style={styles.camera} ref={cameraRef} />
+            <Button
+                title="Take a picture"
+                onPress={async () => {
+                    if (cameraRef.current) {
+                        const pictureMetadata = await cameraRef.current.takePictureAsync();
+                        console.log("pictureMetadata", pictureMetadata);
+                    } else {
+                        console.log("Error while taking picture");
+                    }
+                }}
+            />
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    camera: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+    },
+});
 
